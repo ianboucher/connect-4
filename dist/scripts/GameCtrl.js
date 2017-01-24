718d1da589126940bffff2474363c10573d453ba
Ian Boucher <i.a.boucher@gmail.com>
2017-01-24 14:09:20 +0000

    Complete basic Connect-4 game

--- dist/scripts/GameCtrl.js
+++ dist/scripts/GameCtrl.js
@@ -8,95 +8,72 @@
             function GameCtrl ($scope, boardFactory)
             {
                 self = this;
+                $scope.winner, $scope.nWinsPlayer1 = 0, $scope.nWinsPlayer2 = 0;
 
-                var player = 1;
-                    // board = {};
+                var nToWin, nCols, nRows, player, nMoves;
 
-                // var bitboard = new Uint8Array(new ArrayBuffer(5 * Math.floor(5/5)));
-                // var ROW_MASK = 1 << (5-1);
-                // console.log(bitboard);
-                //
-                // var printBoard = function (board)
-                // {
-                //     console.log();
-                //     console.log("bit-board looks like this:");
-                //     console.log();
-                //
-                //     var strBoard = "";
-                //     for (var row = 0; row < 5; ++row)
-                //     {
-                //         for (var col = 0; col < 5; ++col)
-                //         {
-                //             strBoard += (board[row]&(ROW_MASK>>>col))? "1" : "0";
-                //         }
-                //         strBoard += "\n";
-                //     }
-                //
-                //     console.log(strBoard);
-                // };
-                //
-                // printBoard(bitboard);
-
-                var board = boardFactory.initBitBoard(5, 5);
-                console.log(board.setPosition(1, 4, 1));
-                console.log(board.setPosition(1, 4, 2));
-                console.log(board.setPosition(1, 4, 3));
-                console.log(board.setPosition(1, 4, 4));
-
-                console.log(board.printBoard());
-
-                console.log(board.getPosition(1))
-
-                var hasWon = function (board)
+                (self.newGame = function()
                 {
-                    var y;
+                    nToWin = 4, nCols = 7, nRows = 6, player = 1, nMoves = 0;
+                    $scope.winner = null;
+                    self.board = boardFactory.initialize(nCols, nRows, nToWin)
+                })();
 
-                    y = board & (board >> 5);
-                    if (y & (y >> 2 * 5))
-                    { // check \ diagonal
-                        return true;
+
+                self.dropToken = function(col)
+                {
+                    var emptyRow = self.board[col].lastIndexOf(0);
+
+                	if (emptyRow >= 0 && !$scope.winner)
+                	{
+                		self.board[col][emptyRow] = player;
+                		nMoves += 1;
+                	}
+
+                	if (nMoves > 6)
+                	{
+                		if (checkForWin(self.board, player, col, emptyRow))
+                        {
+                            $scope.winner = player;
+                            (player === 1) ? $scope.nWinsPlayer1++ : $scope.nWinsPlayer2++
+                        };
+                	}
+
+                	changePlayer();
+                }
+                
+
+                self.setDiscColor = function(value)
+                {
+                    var style = { "backgroundColor" : "white" };
+
+                    if (value === 1)
+                    {
+                        style = { "backgroundColor" : "red" };
+                    }
+                    else if (value === 2)
+                    {
+                        style = { "backgroundColor" : "yellow" };
                     }
 
-                    y = board & (board >> 6);
-                    if (y & (y >> 2 * 6))
-                    { // check horizontal -
-                        return true;
-                    }
-
-                    y = board & (board >> 7);
-                    if (y & (y >> 2 * 7))
-                    { // check / diagonal
-                        return true;
-                    }
-
-                    y = board & (board >> 1);
-                    if (y & (y >> 2))
-                    { // check vertical |
-                        return true;
-                    }
-                    return false;
+                    return style;
                 }
 
-                console.log(hasWon(board));
+                var checkForWin = function (board, player, currentCol, currentRow)
+                {
+                    var result = false;
+
+                    result = boardFactory.checkHorizontal (board, player, currentCol, currentRow)  ||
+                             boardFactory.checkVertical (board, player, currentCol, currentRow)    ||
+                             boardFactory.checkDiagonal (board, player, currentCol, currentRow,  1)||
+                             boardFactory.checkDiagonal (board, player, currentCol, currentRow, -1);
+
+                    return result;
+                }
 
                 var changePlayer = function()
                 {
                     player = (player === 1) ? 2 : 1
-                }
-
-                self.select = function(col, row)
-                {
-                    if (board[col] === undefined)
-                    {
-                        board[col] = [player];
-                        changePlayer();
-                    }
-                    else if (board[col].length < 5)
-                    {
-                        board[col].push(player);
-                        changePlayer();
-                    }
-                    console.log(board);
                 }
             }
         ]);
--- dist/scripts/boardFactory.js
+++ dist/scripts/boardFactory.js
@@ -7,206 +7,133 @@
         .factory("boardFactory", [
             function boardFactory ()
             {
-                var self = this;
+                var self = this, nCols, nRows, nToWin;
 
-                /** @module BitBoard
-                 * Provides BitBoard module for writing board games.
-                 *
-                 * An example of an 8x8 Board is depicted as follows:
-                 *
-                 *             01234567  (X)
-                 *            0........
-                 *            1........
-                 *            2........
-                 *            3........
-                 *            4........
-                 *            5........
-                 *            6........
-                 *            7........
-                 *
-                 *           (Y)
-                 */
-                //==========================================================//
-                // static constant variables shared by all BitBoard objects //
-                //==========================================================//
-
-                /** @private */
-                var width;
-
-                /** @private */
-                var height;
-
-                /** @private - row mask of board row */
-                var ROW_MASK;
-
-                /**
-                 * Represents a BitBoard: https://chessprogramming.wikispaces.com/Bitboards
-                 *
-                 * @constructor
-                 * @class
-                 * @param {number} w - width of this bitboard
-                 * @param {number} h - height this bitboard
-                 */
-                function BitBoard (w, h)
+                self.initialize = function (cols, rows, nWin)
                 {
-                    width      = w;
-                    height     = h;
-                    ROW_MASK   = 1 << (width-1);
+                    nCols = cols, nRows = rows, nToWin = nWin;
+                    var array  = [];
 
-                    /** @public */
-                    this.players  = [];
+                    for (var i = 0; i < nCols; i += 1)
+                    {
+                        array.push(Array(nRows).fill(0));
+                    }
 
-                    /** @public */
-                    this.bitboard = new Uint32Array(new ArrayBuffer(w * Math.floor(h/8)));
+                    return array;
                 }
 
-                /** @function setPosition
-                 * Sets a player's position on board.
-                 *
-                 * @param {number} player - the player for which the postion to be set
-                 * @param {number} x - the x coordinate of the postion
-                 * @param {number} y - the y coordinate of the postion
-                 * @return {boolean} - indication of success of set operation
-                 */
-                BitBoard.prototype.setPosition = function (player, x, y)
+                self.checkHorizontal = function (board, player, currentCol, currentRow)
                 {
-                    if ( !checkNonNegativeInt(player) )
+                    var counter = 0, result  = false;
+
+                    for (var i = 0; i < nCols; i += 1)
                     {
-                        throw new Error("function setPosition expects the first argument to be an integer bigger or equal to 0");
+                        if (board[i][currentRow] === player)
+                        {
+                            counter += 1;
+                        }
+                        else
+                        {
+                            counter = 0;
+                        }
+
+                        if (counter === nToWin)
+                        {
+                            result = true;
+                            break;
+                        }
                     }
 
-                    if ( !checkPositiveInt(x) || x >= width )
-                    {
-                        throw new Error("function setPosition expects the second argument to be an integer bigger than 0 and smaller than " + width);
-                    }
+                    return result;
+                }
 
-                    if ( !checkPositiveInt(y) || y >= height )
-                    {
-                        throw new Error("function setPosition expects the third argument to be an integer bigger than 0 and smaller than " + height);
-                    }
-
-                    // check if position already used
-                    if ( this.bitboard[y] & (ROW_MASK >>> x) )
-                        return false;
-
-                    this.bitboard[y]     |= ROW_MASK >>> x;
-                    this.players [player] = y*height + x;
-
-                    return true;
-                };
-
-                /** @function getPosition
-                 * Gets a player's position on board.
-                 *
-                 * @param {number} player - the player for which the postion to be retrieved
-                 * @returns {Object} - the x and y coordinate in Object literal format
-                 */
-                BitBoard.prototype.getPosition = function (player) {
-                    var pos = this.players[player];
-
-                    if ( typeof pos == 'undefined' )
-                        throw new Error('player ' + player + ' is not defined yet!');
-
-                    return {
-                        x: pos % height,
-                        y: Math.floor(pos/height)
-                    };
-                };
-
-                /** @function getBoardCopy
-                 * Copy instance variables for a BitBoard object and return a new object.
-                 *
-                 * @returns {BitBoard} - a new object copy with same instance variable contents
-                 */
-                BitBoard.prototype.getBoardCopy = function () {
-                    var board = new BitBoard(width, height);
-
-                    board.players = this.players.slice();
-
-                    for (var r = 0; r < height; ++r) {
-                        board.bitboard[r] = this.bitboard[r];
-                    }
-
-                    return board;
-                };
-
-                /** @function @public printBoard
-                 * A debugging method for this module; it is exposed to public interface.
-                 * It prints out the board state to console.
-                 *
-                 * @param {BitBoard} - the BitBoard object to be printed
-                 */
-                BitBoard.prototype.printBoard = function ()
+                self.checkVertical = function (board, player, currentCol, currentRow)
                 {
-                    var board = this;
-                    console.log();
-                    console.log("bit-board looks like this:");
-                    console.log();
+                    var counter = 0, result  = false;
 
-                    var strBoard = "";
-                    for (var row = 0; row < height; ++row)
+                    for (var i = 0; i < nCols; i += 1)
                     {
-                        for (var col = 0; col < width; ++col)
+                        if (board[currentCol][i] === player)
                         {
-                            strBoard += (board.bitboard[row]&(ROW_MASK>>>col))? "1" : "0";
+                            counter += 1;
                         }
-                        strBoard += "\n";
+                        else
+                        {
+                            counter = 0;
+                        }
+
+                        if (counter === nToWin)
+                        {
+                            result = true;
+                            break;
+                        }
                     }
 
-                    return strBoard;
-                };
+                    return result;
+                }
 
-                /** @function @private checkNonNegativeInt
-                 * A helper function to safeguard the inputs to some functions in this module.
-                 * It checks if the input value is a none-negative integer.
-                 *
-                 * @param {number} - the input to be checked
-                 * @return {boolean} - check passed or not
-                 */
-                var checkNonNegativeInt = function (x) {
-                    return (typeof x==="number" && Math.floor(x)===x && x >= 0);
-                };
+                self.checkDiagonal = function (board, player, currentCol, currentRow, gradient)
+                {
+                    var startPos = findDiagonalStart(currentCol, currentRow, gradient),
+                        startCol = startPos[0],
+                        startRow = startPos[1],
+                        i = 0, counter = 0, result = false;
 
-                /** @function @private checkPositiveInt
-                 * A helper function to safeguard the inputs to some functions in this module.
-                 * It checks if the input value is a positive integer.
-                 *
-                 * @param {number} - the input to be checked
-                 * @return {boolean} - check passed or not
-                 */
-                var checkPositiveInt = function (x) {
-                    return (typeof x==="number" && Math.floor(x)===x && x > 0);
-                };
-
-                // export module public interface
-                return {
-                    initBitBoard: function(w, h)
+                    while ((startCol + i) < nCols && startRow + (i * gradient) < nRows)
                     {
-                        var i, board;
+                        var cellValue = board[startCol + i][startRow + (i * gradient)];
 
-                        if ( arguments.length != 2 )
+                        if (board[startCol + i][startRow + (i * gradient)] === player)
                         {
-                            throw new Error("function initBitBoard called with " + arguments.length + " arguments, but it expects 2 arguments");
+                            counter += 1;
                         }
-
-                        if ( !checkPositiveInt(w) )
+                        else
                         {
-                            throw new Error("function initBitBoard expects the first argument to be an integer bigger than 0");
+                            counter = 0;
                         }
 
-                        if ( !checkPositiveInt(h) )
+                        if (counter === nToWin)
                         {
-                            throw new Error("function initBitBoard expects the second argument to be an integer bigger than 0");
+                            result = true;
+                            break;
                         }
 
-                        board = new BitBoard(w, h);
-                        for (i = 0; i < height; ++i) {
-                            board.bitboard[i] = 0;
-                        }
-
-                        return board;
+                        i += 1;
                     }
-                };
+
+                    return result;
+                }
+
+
+                // knowing that the 'gradient' of the diagonal must be 1 or -1,
+                // we can use 'straight line' equation, y = mx - b, to determine
+                // start element of current diagonal...
+
+                var findDiagonalStart = function(currentCol, currentRow, gradient)
+                {
+                    var yIntercept = currentRow - (gradient * currentCol),
+                        startCol, startRow;
+
+                    if (yIntercept < 0)
+                    {
+                        startRow = 0;
+                        startCol = 0 - yIntercept;
+                    }
+                    else if (yIntercept > nRows)
+                    {
+                        startRow = nRows;
+                        startCol = yIntercept - nRows;
+                    }
+                    else
+                    {
+                        startRow = yIntercept;
+                        startCol = 0;
+                    }
+
+                    return [startCol, startRow]
+                }
+
+                return self;
             }
         ])
 })();
